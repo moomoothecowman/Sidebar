46dadf09459bee215392664a43e9615062219c14
moomoothecowman <tornadotyler10@outlook.com>
2022-12-23 15:41:50 -0500

    Initial Commit

---
+++ Sidebar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sidebar
+//
+//  Created by Tyler Welch on 12/23/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
